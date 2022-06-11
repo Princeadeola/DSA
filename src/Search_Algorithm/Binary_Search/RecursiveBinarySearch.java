3b70a066fbf5d5ed86ef3d2adcb35b5ff7a67bdc
Adesipe Adeola <56828114+Princeadeola@users.noreply.github.com>
2022-06-11 10:41:44 -0700

    Committed

--- src/Search_Algorithm/Binary_Search/RecursiveBinarySearch.java
+++ src/Search_Algorithm/Binary_Search/RecursiveBinarySearch.java
@@ -1,4 +1,35 @@
 package Search_Algorithm.Binary_Search;
 
 public class RecursiveBinarySearch {
+    public static void main(String[] args) {
+        int[] intArray = { -22, -15, 1, 7, 20, 35, 55 };
+
+        System.out.println(recursiveSearch(intArray, -15));
+        System.out.println(recursiveSearch(intArray, 35));
+        System.out.println(recursiveSearch(intArray, 7));
+        System.out.println(recursiveSearch(intArray, 123));
+        System.out.println(recursiveSearch(intArray, 1));
+
+    }
+
+    public static int recursiveSearch(int[] input, int value){
+        return recursiveSearch(input, 0, input.length, value);
+    }
+
+    public static int recursiveSearch(int[] input, int start, int end, int value){
+        if (start >= end){
+            return -1;
+        }
+
+        int midpoint = (start + end) / 2;
+        System.out.println("midpoint = " + midpoint);
+
+        if (input[midpoint] == value){
+            return midpoint;
+        } else if (input[midpoint] < value) {
+            return recursiveSearch(input, midpoint + 1, end, value);
+        }else {
+            return recursiveSearch(input, start, midpoint, value);
+        }
+    }
 }
