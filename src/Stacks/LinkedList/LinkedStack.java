9c166d3400db11c3c639a52e93514908f5150155
Adesipe Adeola <56828114+Princeadeola@users.noreply.github.com>
2022-07-01 23:39:40 +0100

    Committed

--- src/Stacks/LinkedList/LinkedStack.java
+++ src/Stacks/LinkedList/LinkedStack.java
@@ -1,4 +1,35 @@
 package Stacks.LinkedList;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class LinkedStack {
+    private LinkedList<Employee> stack;
+
+    public LinkedStack(){
+        stack = new LinkedList<Employee>();
+    }
+
+    public void push(Employee employee){
+        stack.push(employee);
+    }
+
+    public Employee pop(){
+        return stack.pop();
+    }
+
+    public Employee peek(){
+        return stack.peek();
+    }
+
+    public boolean isEmpty(){
+        return stack.isEmpty();
+    }
+
+    public void printStack(){
+        Iterator<Employee> iterator = stack.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
 }
--- src/Stacks/LinkedList/Main.java
+++ src/Stacks/LinkedList/Main.java
@@ -3,10 +3,26 @@ package Stacks.LinkedList;
 public class Main {
     public static void main(String[] args) {
 
-       Employee johnDoe = new Employee("John", "Doe", 113);
-       Employee jamesSmith = new Employee("James", "Smith", 233);
-       Employee janeDoe = new Employee("Jane", "Doe", 4563);
-       Employee jackDoe = new Employee("Jack", "Doe", 3421);
+        Employee johnDoe = new Employee("John", "Doe", 113);
+        Employee jamesSmith = new Employee("James", "Smith", 233);
+        Employee janeDoe = new Employee("Jane", "Doe", 4563);
+        Employee jackDoe = new Employee("Jack", "Doe", 3421);
+
+        LinkedStack stack = new LinkedStack();
+        stack.push(johnDoe);
+        stack.push(jamesSmith);
+        stack.push(janeDoe);
+        stack.push(jackDoe);
+
+        System.out.println("Peeked: " + stack.peek());
+        System.out.println("Popped: " + stack.pop());
+
+        stack.printStack();
+        System.out.println("After peeked and popped");
+
+        System.out.println("Peeked: " + stack.peek());
+        System.out.println("Popped: " + stack.pop());
+        stack.printStack();
 
     }
 }
